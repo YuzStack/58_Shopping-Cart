5b69a13b46b7ed183f181f94ce16e16f42844acc
Yusuf Oyinlola <yuzstackinnovation@gmail.com>
2026-03-05 19:41:37 +0100

    Fix Bug: Missing background image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router';
 
 function Hero() {
   return (
-    <section className='relative h-[calc(100vh-64px)] bg-[url(hero.jpg)] bg-cover bg-center bg-no-repeat'>
+    <section className='relative h-[calc(100vh-64px)] bg-[url(/hero.jpg)] bg-cover bg-center bg-no-repeat'>
       <div className='absolute inset-0 z-10 flex items-center justify-center bg-black/40'>
         <div className='max-w-xl text-center'>
           <h1 className='mb-3 text-6xl font-bold tracking-wider text-white uppercase'>

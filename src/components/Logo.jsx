92cb4b882a2072791ceb261f4c920b45505477d3
Yusuf Oyinlola <yuzstackinnovation@gmail.com>
2026-03-05 20:49:47 +0100

    Edit the brand/logo name

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,8 +1,6 @@
 function Logo() {
   return (
-    <h1 className='text-lg font-bold tracking-widest uppercase'>
-      🛒 TrendSetter
-    </h1>
+    <h1 className='text-lg font-bold tracking-widest uppercase'>TrendSetter</h1>
   );
 }
 
